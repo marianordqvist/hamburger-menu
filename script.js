41d46a06f32580456fe0971900ffca7f0ea8a949
marianordqvist <marianordqvist@live.se>
2023-11-22 16:22:49 +0100

    dark / light mode

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+let modeToggleButton = document.querySelector("[data-theme-toggle]");
 let menuEl = document.querySelector(".menu");
 let menuItemsEl = document.querySelectorAll(".menu-item");
 let hamburgerButtonEl = document.querySelector(".hamburger-button");
 let closeIconEl = document.querySelector(".close-icon");
 let menuIconEl = document.querySelector(".menu-icon");
+
+// Function to toggle between light and dark themes
+function toggleTheme() {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  const newTheme = currentTheme === "light" ? "dark" : "light";
+
+  document.documentElement.setAttribute("data-theme", newTheme);
+}
 
 function toggleMenu() {
   if (menu.classList.contains("menu-show")) {
@@ -15,6 +24,8 @@ function toggleMenu() {
     menuIconEl.style.display = "none";
   }
 }
+
+modeToggleButton.addEventListener("click", toggleTheme);
 
 hamburgerButtonEl.addEventListener("click", toggleMenu);
 
